1bdfe9f6b76c544d6f835d30cafd1fb39cc080b7
alex <nap8099@gmail.com>
2024-10-20 21:03:59 +0300

    профиль адаптивный

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -88,8 +88,10 @@ export async function renderProfile() {
     if (!user) {
       throw new Error("Пользователь не найден");
     }
-    const doc: any = document.body;
-    doc.style.height = "100vh";
+    // const doc: any = document.body;
+    // doc.style.height = "100%";
+    const container: any = document.createElement(ELEMENTS.DIV);
+    container.classList.add("main-content");
 
     const formProfile = document.createElement(ELEMENTS.DIV);
     formProfile.classList.add(ELEMENTS_CLASS.FORM_PROFILE);
@@ -110,24 +112,17 @@ export async function renderProfile() {
     right.appendChild(renderUserStats(user));
 
     right.appendChild(renderUserPosts(user));
-    // if (window.location.pathname == "/feed/profile") {
-    //   const containerCreatePosts: any = document.createElement(ELEMENTS.DIV);
-    //   containerCreatePosts.classList.add("create-posts");
-    //   const createButton: any = document.createElement(ELEMENTS.A);
-    //   createButton.classList.add("create-btn")
-    //   createButton.textContent = "Создать"
-    //   containerCreatePosts.appendChild(createButton)
-
-    //   right.appendChild(containerCreatePosts);
-    // }
     renderCreatePost(right);
     profile.appendChild(right);
 
-    formProfile.appendChild(renderSidebar());
+    const sideBar: any = renderSidebar();
+
     formProfile.appendChild(header);
     formProfile.appendChild(profile);
     console.log(formProfile);
-    return formProfile;
+    container.appendChild(formProfile);
+    container.appendChild(sideBar);
+    return container;
   } catch (error) {
     console.log("EROR");
     throw error;
