fd15cd06b7aed46599df53af2025c767ac3d465b
Olga Khoven <olgahoven@gmail.com>
2024-11-03 18:03:17 +0300

    create vdom

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -4,6 +4,10 @@
 
 import { updatePasswordStrengthBar } from "../../auth/signup";
 import { REGEXP, validatePassword } from "../../consts";
+import { renderSidebar } from "../feed/feedView";
+import { getCurrentUser } from "../profile/profile";
+import { createElement, createText, update } from "../../lib/vdom/lib";
+import { VNode } from "../../lib/vdom/src/source";
 
 //TODO добавить обработку старого пароля
 
--- src/pages/settings/settingsView.ts
+++ src/pages/settings/settingsView.ts
@@ -1,7 +1,11 @@
 import { ELEMENTS_CLASS, state } from "../../consts";
+import { createElement, createText, update } from "../../lib/vdom/lib";
+import { VNode } from "../../lib/vdom/src/source";
 import { renderSidebar } from "../feed/feedView";
 import { getCurrentUser } from "../profile/profile";
 import { validateSettingsPassword, validateMainInfo } from "./settings";
+import { pageContainer } from "../../index";
+
 export async function renderSettings() {
   try {
     const user: any | null = await getCurrentUser(window.location.pathname);
@@ -13,26 +17,23 @@ export async function renderSettings() {
 
     state.currentUser = user;
 
-    const sidebar: any = renderSidebar();
+    const vdom: VNode = createElement("div", { class: "container" }, [
+      createElement("h1", {}, [createText("Настройки")]),
+      createElement("div", { class: "tabs" }, [createTabs()]),
+      createElement(
+        "div",
+        { class: "content-container", ref: "contentContainer" },
+        []
+      ),
+      renderSidebar(),
+    ]);
 
-    const container = createContainer();
+    const container = update(pageContainer, vdom); 
+    createTabs(); 
 
-    const mainContent = createMainContent();
-
-    const tabs = createTabs();
-    const contentContainer = createContentContainer();
-
-    setupTabs(tabs, contentContainer);
-    updateContent(contentContainer, 0);
-
-    container.appendChild(tabs);
-    container.appendChild(contentContainer);
-
-    mainContent.appendChild(container);
-    mainContent.appendChild(sidebar);
-    return mainContent;
+    return container;
   } catch (error) {
-    console.log("EROR");
+    console.error("Ошибка:", error);
     throw error;
   }
 }
@@ -52,281 +53,159 @@ function createMainContent(): HTMLDivElement {
   return mainContent;
 }
 
-function createTabs(): HTMLDivElement {
-  const tabs = document.createElement("div");
-  tabs.className = "tabs";
-  return tabs;
-}
+function createTabs(): VNode {
+  const tabNames = ["Основная информация", "Безопасность", "Получение дохода"];
+  const tabs: VNode[] = tabNames.map((tabName, index) =>
+    createElement(
+      "a",
+      {
+        href: "#",
+        class: index === 0 ? "active" : "",
+        events: {
+          click: (event: MouseEvent) => {
+            event.preventDefault();
 
-function createContentContainer(): HTMLDivElement {
-  const contentContainer = document.createElement("div");
-  contentContainer.className = "content-container";
-  return contentContainer;
-}
+            const links = document.querySelectorAll(".tabs a");
+            links.forEach((link) => link.classList.remove("active"));
+            (event.currentTarget as HTMLElement).classList.add("active");
 
-function setupTabs(tabs: HTMLDivElement, contentContainer: HTMLDivElement) {
-  ["Основная информация", "Безопасность", "Получение дохода"].forEach(
-    (tabName, index) => {
-      const tabLink = document.createElement("a");
-      tabLink.href = "#";
-      tabLink.textContent = tabName;
-      tabLink.className = index === 0 ? "active" : "";
-
-      // Обработчик события клика
-      tabLink.addEventListener("click", (event) => {
-        event.preventDefault();
-        tabs
-          .querySelectorAll("a")
-          .forEach((link) => link.classList.remove("active"));
-        tabLink.classList.add("active");
-        updateContent(contentContainer, index);
-      });
-      tabs.appendChild(tabLink);
-    },
+            updateContent(
+              document.querySelector(".content-container") as HTMLElement,
+              index,
+            );
+          },
+        },
+      },
+      [createText(tabName)],
+    ),
   );
+
+  return createElement("div", { class: "tabs" }, tabs);
 }
 
-function updateContent(contentContainer: HTMLDivElement, index: number) {
-  contentContainer.innerHTML = "";
+function updateContent(contentContainer: HTMLElement, index: number) {
+  let vdomContent;
+
   switch (index) {
     case 0:
-      contentContainer.appendChild(createProfileForm());
+      vdomContent = createProfileForm(); 
       break;
     case 1:
-      contentContainer.appendChild(createSecurityForm());
+      vdomContent = createSecurityForm(); 
       break;
     default:
-      console.error("Страница не найдена", index);
+      vdomContent = createElement("div", {}, [
+        createText("Страница не найдена"),
+      ]);
   }
 }
-
-function createProfileForm(): HTMLDivElement {
-  const formContainer = document.createElement("div");
-  formContainer.className = "form-container";
-
-  const formTitle = document.createElement("h2");
-  formTitle.textContent = "Персонализируйте свою страницу";
-  formContainer.appendChild(formTitle);
-
-  const usernameRow = document.createElement("div");
-  usernameRow.className = "form-row";
-  const usernameLabel = createLabel("Имя пользователя", "username");
-  const usernameInput = createInput("text", "username");
-  const usernameError = createErrorMessage();
-  usernameRow.append(usernameLabel, usernameInput);
-  formContainer.append(usernameRow, usernameError);
-
-  const emailRow = document.createElement("div");
-  emailRow.className = "form-row";
-  const emailLabel = createLabel("Электронная почта", "email");
-  const emailInput = createInput("email", "email");
-  const emailError = createErrorMessage();
-  emailRow.append(emailLabel, emailInput);
-  formContainer.append(emailRow, emailError);
-
-  const roleRow = document.createElement("div");
-  roleRow.className = "form-row";
-  const roleLabel = createLabel("Роль", "role");
-  const roleSelect = createRoleSelect();
-  roleRow.append(roleLabel, roleSelect);
-
-  const profilePicRow = createPhoto();
-
-  const saveButton = document.createElement("button");
-  saveButton.className = ELEMENTS_CLASS.SEND_TIP.COMBINE;
-  saveButton.textContent = "Сохранить";
-
-  formContainer.addEventListener("input", (event) => {
-    event.preventDefault();
-
-    const { usernameError: usernameErrorMsg, emailError: emailErrorMsg } =
-      validationMainInfoSave(usernameInput.value, emailInput.value);
-    usernameError.textContent = usernameErrorMsg || "";
-    emailError.textContent = emailErrorMsg || "";
-  });
-
-  formContainer.append(roleRow, profilePicRow, saveButton);
-
-  return formContainer;
+function createInput(type: string, id: string): VNode {
+  return createElement("input", { type, id }, []);
 }
 
-export function validationMainInfoSave(
-  username: string,
-  email: string,
-): { usernameError: string; emailError: string } {
-  const { usernameError, emailError } = validateMainInfo(username, email);
-  return { usernameError, emailError };
+function createLabel(text: string, htmlFor: string): VNode {
+  return createElement("label", { htmlFor }, [createText(text)]);
 }
 
-function createSecurityForm(): HTMLDivElement {
-  const formContainer = document.createElement("div");
-  formContainer.className = "form-container";
+function createProfileForm() {
+  const vdom: VNode = createElement("div", { class: "profile-form" }, [
+    createElement("div", { class: "form-container" }, [
+      createElement("h2", {}, [createText("Персонализируйте свою страницу")]),
 
-  const formTitle = document.createElement("h2");
-  formTitle.textContent = "Измените данные вашей страницы";
-  formContainer.appendChild(formTitle);
+      createElement("div", { class: "form-row" }, [
+        createText("Имя пользователя"),
+        createInput("text", "username"), 
+      ]),
+      createErrorMessage(),
 
-  const oldPasswordRow = document.createElement("div");
-  oldPasswordRow.className = "form-row";
-  const oldPasswordLabel = createLabel("Введите старый пароль", "old-password");
-  const oldPasswordInput = createInput("password", "old-password");
-  const oldPasswordError = createErrorMessage();
-  oldPasswordRow.append(oldPasswordLabel, oldPasswordInput);
-  formContainer.append(oldPasswordRow, oldPasswordError);
+      createElement("div", { class: "form-row" }, [
+        createText("Электронная почта"),
+        createInput("email", "email"), 
+      ]),
+      createErrorMessage(),
 
-  const newPasswordRow = document.createElement("div");
-  newPasswordRow.className = "form-row";
-  const passwordStrength = document.createElement("div");
-  passwordStrength.className = "password-strength";
-  const newPasswordLabel = createLabel("Введите новый пароль", "new-password");
-  const newPasswordInput = createInput("password", "new-password");
-  const newPasswordError = createErrorMessage();
-  newPasswordRow.append(newPasswordLabel, newPasswordInput);
-  formContainer.append(newPasswordRow, newPasswordError);
+      createElement("div", { class: "form-row" }, [
+        createText("Выберите вашу роль:"),
+        createRoleButton(), 
+      ]),
 
-  const confirmPasswordRow = document.createElement("div");
-  confirmPasswordRow.className = "form-row";
-  const confirmPasswordLabel = createLabel(
-    "Повторите пароль",
-    "confirm-password",
-  );
-  const confirmPasswordInput = createInput("password", "confirm-password");
-  const confirmPasswordError = createErrorMessageStrength();
-  confirmPasswordRow.append(confirmPasswordLabel, confirmPasswordInput);
-  formContainer.append(confirmPasswordRow, confirmPasswordError);
+      createPhoto(),
 
-  const saveButton = document.createElement("button");
-  saveButton.className = ELEMENTS_CLASS.SEND_TIP.COMBINE;
-  saveButton.textContent = "Сохранить";
+      createElement("button", { class: ELEMENTS_CLASS.SEND_TIP.COMBINE }, [
+        createText("Сохранить"),
+      ]),
+    ]),
+  ]);
 
-  formContainer.addEventListener("input", (event) => {
-    event.preventDefault();
-    validationSecuritySave(
-      oldPasswordInput,
-      newPasswordInput,
-      confirmPasswordInput,
-      oldPasswordError,
-      newPasswordError,
-      confirmPasswordError,
-    );
-  });
-
-  formContainer.append(
-    oldPasswordRow,
-    oldPasswordError,
-    newPasswordRow,
-    newPasswordError,
-    confirmPasswordRow,
-    confirmPasswordError,
-    saveButton,
-  );
-
-  return formContainer;
+  return vdom;
 }
 
-function validationSecuritySave(
-  oldPasswordInput: HTMLInputElement,
-  newPasswordInput: HTMLInputElement,
-  confirmPasswordInput: HTMLInputElement,
-  oldPasswordError: HTMLDivElement,
-  newPasswordError: HTMLDivElement,
-  confirmPasswordError: HTMLDivElement,
-): void {
-  oldPasswordError.textContent = "";
-  newPasswordError.textContent = "";
-  confirmPasswordError.textContent = "";
+function createSecurityForm(): VNode {
+  const vdom: VNode = createElement("div", { class: "profile-form" }, [
+    createElement("div", { class: "security-form" }, [
+      createElement("div", { class: "form-container" }, [
+        createElement("h2", {}, [createText("Измените данные вашей страницы")]),
 
-  let passwordStrength = 0; // Переменная для хранения силы пароля
-  validateSettingsPassword(
-    newPasswordInput,
-    confirmPasswordInput,
-    passwordStrength,
-    newPasswordError,
-    confirmPasswordError,
+        createElement("div", { class: "form-row" }, [
+          createLabel("Введите старый пароль", "old-password"),
+          createInput("password", "old-password"),
+        ]),
+        createErrorMessage(),
+
+        createElement("div", { class: "form-row" }, [
+          createElement("div", { class: "password-strength" }, [
+            createLabel("Введите новый пароль", "new-password"),
+            createInput("password", "new-password"),
+          ]),
+        ]),
+        createErrorMessage(),
+
+        createElement("div", { class: "form-row" }, [
+          createLabel("Повторите пароль", "confirm-password"),
+          createInput("password", "confirm-password"),
+        ]),
+        createErrorMessageStrength(),
+
+        createElement("button", { class: ELEMENTS_CLASS.SEND_TIP.COMBINE }, [
+          createText("Сохранить"),
+        ]),
+      ]),
+    ]),
+  ]);
+
+  return vdom;
+}
+
+function createRoleButton(): VNode {
+  return createElement("button", { class: "become-author-button" }, [
+    createText("Стать автором"),
+  ]);
+}
+
+function createPhoto(): VNode {
+  return createElement("div", { class: "photo-form" }, [
+    createElement("label", { for: "profile-pic" }, [
+      createText("Фото профиля"),
+    ]),
+    createElement("div", { class: "profile-pic-container" }, [
+      createElement("img", { class: "profile-pic", src: "" }, []),
+      createElement(
+        "input",
+        { type: "file", id: "profile-pic", accept: "image/*" },
+        [],
+      ),
+    ]),
+  ]);
+}
+
+function createErrorMessage(): VNode {
+  return createElement(
+    "div",
+    { class: "error-message", style: "color: red;" },
+    [],
   );
 }
 
-function createLabel(text: string, htmlFor: string): HTMLLabelElement {
-  const label = document.createElement("label");
-  label.setAttribute("for", htmlFor);
-  label.textContent = text;
-  return label;
-}
-
-function createInput(type: string, id: string): HTMLInputElement {
-  const input = document.createElement("input");
-  input.type = type;
-  input.id = id;
-  return input;
-}
-
-function createRoleSelect(): HTMLSelectElement {
-  const roleSelect = document.createElement("select");
-  roleSelect.id = "role";
-
-  const defaultOption = document.createElement("option");
-  defaultOption.value = "";
-  defaultOption.textContent = "Выберите роль";
-  defaultOption.disabled = true;
-  defaultOption.selected = true;
-  roleSelect.appendChild(defaultOption);
-
-  const roles = ["Автор", "Читатель"];
-  roles.forEach((role) => {
-    const option = document.createElement("option");
-    option.value = role.toLowerCase();
-    option.textContent = role;
-    roleSelect.appendChild(option);
-  });
-
-  return roleSelect;
-}
-
-function createPhoto(): HTMLDivElement {
-  const profilePicLabel = document.createElement("label");
-  profilePicLabel.setAttribute("for", "profile-pic");
-  profilePicLabel.textContent = "Фото профиля";
-
-  const profilePicDiv = document.createElement("div");
-  profilePicDiv.className = "profile-pic-container";
-
-  const profilePic = document.createElement("img");
-  profilePic.className = "profile-pic";
-
-  const profilePicInput = document.createElement("input");
-  profilePicInput.type = "file";
-  profilePicInput.id = "profile-pic";
-  profilePicInput.accept = "image/*";
-
-  profilePicInput.addEventListener("change", (event: any) => {
-    const file = event.target.files[0];
-    if (file) {
-      const reader = new FileReader();
-      reader.onload = function (e: any) {
-        profilePic.src = e.target.result;
-      };
-      reader.readAsDataURL(file);
-    }
-  });
-
-  profilePicDiv.appendChild(profilePic);
-  profilePicDiv.appendChild(profilePicInput);
-
-  const container = document.createElement("div");
-  container.appendChild(profilePicLabel);
-  container.appendChild(profilePicDiv);
-
-  return container;
-}
-
-function createErrorMessage(): HTMLDivElement {
-  const errorMessage = document.createElement("div");
-  errorMessage.className = "error-message";
-  errorMessage.style.color = "red";
-  return errorMessage;
-}
-function createErrorMessageStrength(): HTMLDivElement {
-  const errorMessage = document.createElement("div");
-  errorMessage.className = "password-strength";
-  return errorMessage;
+function createErrorMessageStrength(): VNode {
+  return createElement("div", { class: "password-strength" }, []);
 }
