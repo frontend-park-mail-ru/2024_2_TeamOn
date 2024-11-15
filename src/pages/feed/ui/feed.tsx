d84f09c5bfa9c60cd72565f185e9272cc3583cb5
alex <nap8099@gmail.com>
2024-11-16 00:33:06 +0300

    управление сайдбаром

--- src/pages/feed/ui/feed.tsx
+++ src/pages/feed/ui/feed.tsx
@@ -7,7 +7,7 @@ import { Sidebar } from "../../../shared/sidebar/sidebar";
 
 import * as VDom from "vdom";
 
-export async function createAppVNode() {
+async function renderFeedForm() {
   const userdata = await getAccount();
   return (
     <div class="main-content">
@@ -22,41 +22,4 @@ export async function createAppVNode() {
   );
 }
 
-// /**
-//  * Функция рендера сайдбара
-//  * @param userdata Информация о юзере
-//  * @returns
-//  */
-// export async function Sidebar(userdata: any) {
-//   sessionStorage.setItem("account", userdata.username);
-//   const role = userdata.role;
-//   const styleProfileIcon = role === "Reader" ? "display: none" : "";
-//   return (
-//     <div class="side">
-//       <div class="burger">
-//         <div class="line"></div>
-//         <div class="line"></div>
-//         <div class="line"></div>
-//       </div>
-//       <div class="sidebar">
-//         <div class="nav-menu">
-//           <a class="referens">
-//             <i class="icon-home"></i>
-//             Главная
-//           </a>
-//           <a class="referens">
-//             <i class="icon-settings"></i>
-//             Настройки
-//           </a>
-//           <a class="referens profile" style={styleProfileIcon}>
-//             <i class="icon-profile"></i>
-//             Профиль
-//           </a>
-//         </div>
-//         <div class="logout">
-//           <div class="logout__button logout__button__effects">Выйти</div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
+export { renderFeedForm }
