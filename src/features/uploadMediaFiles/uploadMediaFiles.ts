5d14768023dcee45b1f5f2acd996ef8f90feed80
alex <nap8099@gmail.com>
2024-11-25 17:53:28 +0300

    Поправил баги

--- src/features/uploadMediaFiles/uploadMediaFiles.ts
+++ src/features/uploadMediaFiles/uploadMediaFiles.ts
@@ -14,52 +14,21 @@ async function uploadMediaFiles(postId: number, files: File[]) {
       formData.append(`file${index + 1}`, file);
     });
 
-    // Создаем объект для отслеживания статуса
-    const uploadStatus = { pending: true };
-
     fetchAjax(
       "POST",
       "/api/posts/post/upload/media/" + `${postId}`,
       formData,
       (response) => {
-        uploadStatus.pending = false; // Изменяем статус на "не в ожидании"
         if (response.ok) {
           resolve({ success: true, pending: false });
         } else if (response.status === 400) {
-          resolve({ success: false, pending: false });
+          resolve(false);
         } else {
-          reject(new Error("Внутренняя ошибка сервера"));
+          resolve(false)
         }
       },
     );
-
-    // Возвращаем статус загрузки
-    resolve(uploadStatus);
   });
 }
-// async function uploadMediaFiles(postId: number, files: File[]) {
-//   return new Promise((resolve, reject) => {
-//     const formData = new FormData();
-//     files.forEach((file: any, index: number) => {
-//       formData.append(`file${index + 1}`, file); // Используем "files[]" для отправки массива файлов
-//     });
-
-//     fetchAjax(
-//       "POST",
-//       "/api/posts/post/upload/media/" + `${postId}`,
-//       formData, // Передаем formData напрямую
-//       (response) => {
-//         if (response.ok) {
-//           resolve(true);
-//         } else if (response.status === 400) {
-//           resolve(false);
-//         } else {
-//           resolve(false);
-//           // reject(new Error("Внутреняя ошибка сервера"));
-//         }
-//       },
-//     );
-//   });
-// }
 
 export { uploadMediaFiles };
